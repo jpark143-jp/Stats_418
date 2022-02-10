3b615fece2cb5e183775985c6505c6d4f2a2324f
jpark143-jp <jpark143@g.ucla.edu>
2022-02-10 11:08:16 -0800

    Add files via upload

--- hw1/q2/stats418hello904615627world.py
+++ hw1/q2/stats418hello904615627world.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    time = datetime.datetime.now()
+    currenthour = time.hour
+    if 6 <= currenthour < 12:
+        return "Hello World! Good morning"
+    elif 12 <= currenthour < 18:
+        return "Hello World! Good afternoon"
+    else:
+        return "Hello World! Good night"
+    
