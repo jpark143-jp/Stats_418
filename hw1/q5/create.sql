6214b6ef59e59be6ce321248862a041dfc43193c
jpark143-jp <jpark143@g.ucla.edu>
2022-02-10 10:43:11 -0800

    Update create.sql

--- hw1/q5/create.sql
+++ hw1/q5/create.sql
@@ -1,9 +1,12 @@
--- p. 5.1
--- 1. Creating a db
+-- Probset 1 
+-- 5.1
+
+
+-- Create database
 CREATE DATABASE hw1;
 
--- 2. Creating tables
--- Creating a table just like the csv.file
+-- Initiate tables
+
 CREATE TABLE hw1.sales_test (
 	'Date' as DATE
     , 'S1' INT
@@ -19,7 +22,7 @@ CREATE TABLE hw1.sales_test (
 	, PRIMARY KEY ('Date')
 );
 
--- Transforming sales_test table structure to create sales_data table 
+-- Create sales_data table 
 CREATE TABLE hw1.sales_data (
 	'sale_date' as DATE
     , 'store_ID' STRING
@@ -28,7 +31,7 @@ CREATE TABLE hw1.sales_data (
     , FOREIGN KEY ('store_ID')
 );
 
--- Creating a store table (Since we don't have store information, we will leave it blank.)
+-- Create a store table
 CREATE TABLE hw1.store_info (
 	'store_ID' STRING
     , 'store_name' VARCHAR(255)
@@ -39,7 +42,7 @@ CREATE TABLE hw1.store_info (
     , PRIMARY KEY ('store_ID')
 );
 
--- 2. Import TV_Sales.csv
+-- TV_Sales.csv
 LOAD DATA INFILE 'c:/TV_Sales.csv' 
 INTO TABLE sales_test
 FIELDS TERMINATED BY ',' 
@@ -47,62 +50,72 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 
--- 3. Importing values from sales_test to sales_data
--- 3.1 s1
+-- 3. sales_test -> sales_data
+
+-- S1
 CREATE TABLE hw1.sales_data AS
 SELECT date	AS sale_date
 	, 's1' AS store
     , s1 AS sales
 FROM hw1.store_sales
--- 3.2 s2
+
+-- S2
 UNION 
 SELECT date	AS sale_date
 	, 's2' AS store
     , s2 AS sales
 FROM hw1.store_sales
--- 3.3 s3
+
+-- S3
 UNION 
 SELECT date	AS sale_date
 	, 's3' AS store
     , s3 AS sales
 FROM hw1.store_sales
--- 3.4 s.4
+
+-- S4
 UNION 
 SELECT date	AS sale_date
 	, 's4' AS store
     , s4 AS sales
 FROM hw1.store_sales
--- 3.5 s.5
+
+-- S5
 UNION 
 SELECT date	AS sale_date
 	, 's5' AS store
     , s5 AS sales
 FROM hw1.store_sales
--- 3.6 s.6
+
+-- S6
 UNION 
 SELECT date	AS sale_date
 	, 's6' AS store
     , s6 AS sales
 FROM hw1.store_sales
--- 3.7 s.7
+
+-- S7
 UNION
 SELECT date	AS sale_date
 	, 's7' AS store
     , s7 AS sales
 FROM hw1.store_sales
--- 3.8 s.8
+
+-- S8
 UNION 
 SELECT date	AS sale_date
 	, 's8' AS store
     , s8 AS sales
 FROM hw1.store_sales
--- 3.9 s.9
+
+-- S9
 UNION 
 SELECT date	AS sale_date
 	, 's9' AS store
     , s9 AS sales
 FROM hw1.store_sales
--- 3.10 s.10
+
+-- S10
 UNION 
 SELECT date	AS sale_date
 	, 's10' AS store
