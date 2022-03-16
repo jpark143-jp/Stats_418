819c9b81d86c63a192449380cec990ec1fa88182
jpark143-jp <jpark143@g.ucla.edu>
2022-03-15 22:25:37 -0700

    Update and rename Logistic_regression.py to .gitkeep

--- hw2/q4/Logistic_regression.py
+++
@@ -1,9 +0,0 @@
-# Logistic Regression Model fitting
-
-LR=LogisticRegression(C=100, solver='lbfgs', max_iter=1000)
-LR.fit(x_train, np.ravel(y_train))
-
-score_train=LR.score(x_train,y_train)
-score_test=LR.score(x_test,y_test)
-y_pred = LR.predict(x_test)
-cv = cross_val_score(LR, x_test, y_test).mean()
